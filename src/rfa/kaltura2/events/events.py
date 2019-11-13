fe990371ef8bed8c8d1927c4f88862076ae54108
flipmcf <flipmcf@gmail.com>
2019-11-13 14:45:06 -0500

    modified event works - except file is always flagged as modified

--- src/rfa/kaltura2/events/events.py
+++ src/rfa/kaltura2/events/events.py
@@ -1,5 +1,9 @@
+import logging
+
 
 from KalturaClient.Plugins.Core import KalturaEntryModerationStatus
+from KalturaClient.Plugins.Core import KalturaUploadedFileTokenResource
+from KalturaClient.exceptions import KalturaException
 
 from rfa.kaltura2.kutils import kconnect
 from rfa.kaltura2.kutils import KalturaLoggerInstance as logger
@@ -11,6 +15,7 @@ from rfa.kaltura2.kutils import setModerationStatus
 from rfa.kaltura2.kutils import syncCategories
 
 from rfa.kaltura2.adapters.kalturaMediaEntry import IKalturaMediaEntryProvider
+from rfa.kaltura2.kaltura_video import IKaltura_Video
 
 def addVideo(context, event):
     """When a video is added to a container
@@ -38,7 +43,43 @@ def modifyVideo(context, event):
        Any differences between plone object (context) and kaltura object
        are considered edits to the kaltura object, and are sent to kaltura
     """
-    pass
+    newMediaEntry = IKalturaMediaEntryProvider(context).getEntry()
+    entryId = context.KalturaObject.getId()
+    file_changed = False
+    if hasattr(event, 'descriptions') and event.descriptions:
+        for d in event.descriptions:
+            if d.interface is IKaltura_Video and 'video_file' in d.attributes:
+                file_changed = True
+                
+    (client, session) = kconnect()
+    
+    mediaEntry = client.media.update(entryId, newMediaEntry)
+    context.KalturaObject = mediaEntry
+    
+    if file_changed:
+        logger.log('uploading new video for %s' % (context.getId(),),
+               level=logging.WARN)
+    
+        uploadTokenId = uploadVideo(context, client)
+        resource = KalturaUploadedFileTokenResource()
+        resource.setToken(uploadTokenId)
+        try:
+            client.media.updateContent(entryId, resource)
+        except KalturaException as e:
+            if e.code == 'ENTRY_REPLACEMENT_ALREADY_EXISTS':
+                #auto-deny the half-cooked replacement and re-try
+                client.media.cancelReplace(entryId)
+                client.media.updateContent(entryId, resource)
+
+        #XXX if 'auto approve' is turned off in settings:
+        try:
+            newMediaEntry = client.media.approveReplace(entryId)
+        except KalturaException as e:
+            if e.code == 'ENTRY_ID_NOT_REPLACED':
+                logger.log('Kaltura Video not replaced on KMC for %s, entry id %s' %(context.getId(), entryId),
+                           level=logging.WARN)
+                   
+    syncCategories(context, client)     
     
 def deleteVideo(context, event):
     ##TODO - configure option to delete or reject plone deleted content.
