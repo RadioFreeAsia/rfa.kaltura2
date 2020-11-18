6786b9193933ecd379c69de495c26788b2745dc6
bizuyghur <Oglum730>
2020-11-18 14:27:18 -0500

    Schema and behavior update. #2506

--- src/rfa/kaltura2/kaltura_video.py
+++ src/rfa/kaltura2/kaltura_video.py
@@ -17,28 +17,28 @@ from rfa.kaltura2 import _
 
 
 class IKaltura_Video(model.Schema):
-    
+
     title = schema.TextLine(
-        title=_('video name'),
-        description=_('The short name of this video')
+        title=_('Video Title'),
+        description=_('Title of your video')
     )
-    
+
     description = schema.Text(
-        title=_('video summary'),
+        title=_('Video summary'),
     )
-    
+
     player = schema.Choice(
         title=_('Player'),
         description=_('Choose the player to use'),
         source=VideoPlayerVocabularyFactory
         )
-    
+
     categories = schema.List(
         title=_('Categories'),
         description=_("Select video category(ies) this video belongs in"),
         value_type=schema.Choice(source=CategoryVocabularyFactory(None))
     )
-    
+
     tags = schema.Tuple(
         title=_('Tags'),
         description=_("keyword tag(s) associated with this video (one per line)"),
@@ -51,21 +51,21 @@ class IKaltura_Video(model.Schema):
         AjaxSelectFieldWidget,
         vocabulary='plone.app.vocabularies.Keywords'
     )
-    
+
     video_file = NamedBlobFile(
         title=_('video file'),
     )
-    
+    model.primary('video_file')
     ##Hidden Fields
     #User never interacts with these fields
-    directives.omitted('entryId', 'KalturaObject') 
+    directives.omitted('entryId', 'KalturaObject')
     entryId = schema.Text(
         title=_('entryid'),
         description=_('Entry Id set by Kaltura after upload (read only)'),
         default=None
     )
-    
+
     #This stores the python object representing the entry in the Kaltura Media Center
     KalturaObject = schema.Object(IKalturaMediaEntry)
-    
-    
+
+
