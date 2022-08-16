841a08c517a1e93b9ea55dcda1d715b9495a498e
Arkady Dolina <55853692+adunderground@users.noreply.github.com>
2022-08-16 16:09:57 -0400

    init

---
+++ src/rfa/kaltura2/browser/addForm.py
@@ -0,0 +1,17 @@
+from bdb import set_trace
+from plone.dexterity.browser import add
+from Products.CMFPlone.resources import add_resource_on_request
+
+class AddForm(add.DefaultAddForm):
+    portal_type = 'Kaltura Video'
+    
+    def update(self):
+        return super(AddForm, self).update()
+
+class AddView(add.DefaultAddView):
+    form = AddForm
+    
+    def __call__(self):
+        import pdb; pdb.set_trace()
+        add_resource_on_request(self.request, 'indexJS')
+        return super(AddView, self).__call__()
