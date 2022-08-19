321a0495a3d6e17b392f4e00e715bb5710700082
bizuyghur <zerepshan@gmail.com>
2022-08-19 13:45:08 -0400

    js needs to be one per record in the xml.

--- src/rfa/kaltura2/browser/addForm.py
+++ src/rfa/kaltura2/browser/addForm.py
@@ -12,5 +12,8 @@ class AddView(add.DefaultAddView):
     form = AddForm
 
     def __call__(self):
+
         add_resource_on_request(self.request, 'kalturaFileUploader')
+        add_resource_on_request(self.request, 'filepond')
+        add_resource_on_request(self.request, 'filepondesm')
         return super(AddView, self).__call__()
