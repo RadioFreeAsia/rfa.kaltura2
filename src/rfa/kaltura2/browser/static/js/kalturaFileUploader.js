312b61b8adfbab4fff016c374ef5c9369449ca83
Arkady Dolina <55853692+adunderground@users.noreply.github.com>
2022-08-22 21:10:02 -0400

    add file pond and register components

--- src/rfa/kaltura2/browser/static/js/kalturaFileUploader.js
+++ src/rfa/kaltura2/browser/static/js/kalturaFileUploader.js
@@ -1,24 +1,66 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    const inputElement = document.getElementById('form-widgets-video_file-input');
-    console.log(FilePond);
 
 
-    console.log(inputElement);
-});
 
 function cleanupStyle() {
     const el = document.querySelector('.filepond--credits');
     el.style.display = 'none';
 }
 
+function appendStyle() {
+    const link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = "++plone++rfa.kaltura2/css/filepond.min.css";
+    document.head.appendChild(link);
+}
 
-console.log('123123 123');
-// console.log(inputElement);
+require([
+    "++plone++rfa.kaltura2/js/filepond.min.js",
+    "++plone++rfa.kaltura2/js/filepond-plugin-file-validate-size.js",
+    "++plone++rfa.kaltura2/js/filepond-plugin-file-validate-type.js"],
+    (FilePond, FilePondPluginFileValidateSize, FilePondPluginFileValidateType) => {
 
+        appendStyle();
 
+        const inputElement = document.getElementById('kalturaInput');
 
-// FilePond.registerPlugin(FilePondPluginFileValidateSize);
-// FilePond.registerPlugin(FilePondPluginFileValidateType);
+        FilePond.registerPlugin(FilePondPluginFileValidateSize);
+        FilePond.registerPlugin(FilePondPluginFileValidateType);
+
+        const pond = FilePond.create(inputElement);
+        cleanupStyle();
+
+        pond.setOptions({
+            maxFiles: 1,
+            required: true,
+            allowDrop: false,
+            instantUpload: false,
+            maxFileSize: '2000MB',
+            // acceptedFileTypes: ['video/*'],
+            chunkUploads: true,
+            chunkSize: 10000000,
+            chunkForce: false,
+            chunkRetryDelays: [500, 1000, 3000],
+            server: {
+                url: './',
+                timeout: 7000,
+                process: {
+                    url: '',
+                    method: 'POST',
+                    headers: {
+                        'x-customheader': 'Hello World',
+                    },
+                    withCredentials: false,
+                    onload: (response) => console.log(response),
+                    onerror: (response) => console.log(response),
+                    ondata: (formData) => console.log(formData),
+                },
+                // patch: {
+                //     url: '/'
+                // },
+            },
+        })
+    });
 
 // const pond = FilePond.create(inputElement);
 
