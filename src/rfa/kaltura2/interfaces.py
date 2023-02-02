b6a33ea3e86d6dc397581610d5e245e61ce9d4f1
bizuyghur <zerepshan@gmail.com>
2023-02-02 13:04:59 -0500

    Let us see if this gonna fix the edit problem.  #3284

--- src/rfa/kaltura2/interfaces.py
+++ src/rfa/kaltura2/interfaces.py
@@ -2,7 +2,7 @@
 from zope.interface import Interface
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
-from plone.formwidget.namedfile.interfaces import INamedFileWidget
+from z3c.form.interfaces import ITextWidget
 
 class IRfaKaltura2Layer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
@@ -12,7 +12,7 @@ class IKalturaMediaEntry(Interface):
        This represents an instance of KaltraClient.Plugins.Core.KalturaMediaEntry
     """
 
-class IUploadFileWidget(INamedFileWidget):
+class IUploadFileWidget(ITextWidget):
     """
       An IUploadFileWidget is a special NamedFileWidget that uploads
       the file contents to a remote destination instead of saving it as a blob.
