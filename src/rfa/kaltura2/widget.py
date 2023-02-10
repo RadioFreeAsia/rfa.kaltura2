7364feb286a348f3864cc19f9086a155b8333195
Enver Uyghur <uyghure@rfa.org>
2023-02-10 07:16:02 -0500

    Update widget.py

--- src/rfa/kaltura2/widget.py
+++ src/rfa/kaltura2/widget.py
@@ -1,11 +1,11 @@
 from zope.interface import implementer_only
-from plone.formwidget.namedfile.widget import NamedFileWidget
+from z3c.form.interfaces import ITextWidget
 
 from rfa.kaltura2.interfaces import IUploadFileWidget
 
 
 @implementer_only(IUploadFileWidget)
-class FileUploadWidget(NamedFileWidget):
+class FileUploadWidget(ITextWidget):
     def update(self):
         super(FileUploadWidget, self).update()
 
